a97afa96016de2242cb9c156e26a36a41605d305
DJDavid98 <djdavid98@protonmail.com>
2021-08-25 20:24:41 +0200

    Add auxiliary code for supporting Arabic

--- src/moment-locales.ts
+++ src/moment-locales.ts
@@ -3,6 +3,7 @@ import 'moment/locale/en-gb';
 import 'moment/locale/fr';
 import 'moment/locale/hu';
 import 'moment/locale/ru';
+import 'moment/locale/ar';
 import moment from 'moment-timezone';
 import latestTimezoneData from 'moment-timezone/data/packed/latest.json';
 
