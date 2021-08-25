107d8da3d09d619c4886b96a793fdbeba21ccabf
DJDavid98 <djdavid98@protonmail.com>
2021-08-25 23:35:50 +0200

    Sort moment locales alphabetically

--- src/moment-locales.ts
+++ src/moment-locales.ts
@@ -1,6 +1,6 @@
 import 'moment/locale/en-gb';
-import 'moment/locale/hu';
 import 'moment/locale/fr';
+import 'moment/locale/hu';
 import 'moment/locale/ru';
 import moment from 'moment-timezone';
 import latestTimezoneData from 'moment-timezone/data/packed/latest.json';
