ede6f80ff124da328d6786fd4d19c5113b9d73ed
DJDavid98 <djdavid98@protonmail.com>
2022-03-12 19:35:41 +0100

    Add Discord server redirect

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 const { i18n } = require('./next-i18next.config.js');
+const { redirects } = require('./vercel.json');
 const withPlugins = require('next-compose-plugins');
 const withCamelCaseCSSModules = require('./utils/next-css-modules');
 
 module.exports = withPlugins([[withCamelCaseCSSModules]], {
   i18n,
+  redirects: async () => redirects,
 });
