66dc710d87f753a8011f3e246799a1497c26ffdc
DJDavid98 <djdavid98@protonmail.com>
2024-09-30 00:12:33 +0200

    Make library credits translatable again

--- src/components/app/ExternalLibrariesCredit.tsx
+++ src/components/app/ExternalLibrariesCredit.tsx
@@ -2,14 +2,18 @@ import { FC } from 'react';
 import { ExternalLink } from 'components/ExternalLink';
 import { FONTAWESOME_FREE_LICENSE_URL, MANTINE_URL } from 'src/config';
 import styles from 'modules/ExternalLibrariesCredit.module.scss';
+import { useTranslation } from 'next-i18next';
 
-export const ExternalLibrariesCredit: FC = () => (
-  <ul className={styles['external-libraries-credit']}>
-    <li>
-      <ExternalLink href={FONTAWESOME_FREE_LICENSE_URL}>Font Awesome Free</ExternalLink>
-    </li>
-    <li>
-      <ExternalLink href={MANTINE_URL}>Mantine</ExternalLink>
-    </li>
-  </ul>
-);
+export const ExternalLibrariesCredit: FC = () => {
+  const { t } = useTranslation();
+  return (
+    <ul className={styles['external-libraries-credit']}>
+      <li>
+        <ExternalLink href={FONTAWESOME_FREE_LICENSE_URL}>{t('credits.fontAwesomeFree')}</ExternalLink>
+      </li>
+      <li>
+        <ExternalLink href={MANTINE_URL}>{t('credits.mantine')}</ExternalLink>
+      </li>
+    </ul>
+  );
+};
